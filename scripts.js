4c8c114f736f4bd0fa596444e651cff518530e75
gpmacedo <gpmmacedo@hotmail.com>
2026-06-20 10:30:28 -0400

    Fix grid

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-for (let i = 0; i < 15; i++) {
+for (let i = 0; i < 16 * 16; i++) {
   const grid = document.createElement("div");
   grid.classList.add("square");
   document.querySelector(".grid-container").appendChild(grid);
