af3b595ec95bed595954e088aaabf3daef01a939
gpmacedo <gpmmacedo@hotmail.com>
2026-06-20 11:30:01 -0400

    Fix the hover function

--- scripts.js
+++ scripts.js
@@ -7,13 +7,21 @@ for (let i = 0; i < 16 * 16; i++) {
   container.appendChild(grid);
 }
 
+const allSquares = document.querySelectorAll(".square");
+
+allSquares.forEach((square) => {
+  square.addEventListener("mouseenter", () => {
+    square.style.backgroundColor = "grey";
+  });
+});
+
 btn.addEventListener("click", () => {
   const gridSize = prompt("Escolha o tamanho da grid", "1-100");
   if (gridSize < 1 || gridSize > 100) {
     alert("O valor da grid deve estar entre 1 e 100");
   } else {
     container.innerHTML = "";
-    for (let i = 0; i < gridSize; i++) {
+    for (let i = 0; i < gridSize * gridSize; i++) {
       const grid = document.createElement("div");
       grid.classList.add("square");
       container.appendChild(grid);
