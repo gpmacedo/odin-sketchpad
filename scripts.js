da34e2c6991c95030cc9f2f67801ba474ac23ad9
gpmacedo <gpmmacedo@hotmail.com>
2026-06-20 11:12:03 -0400

    Add custom grid function

--- scripts.js
+++ scripts.js
@@ -12,6 +12,16 @@ btn.addEventListener("click", () => {
   if (gridSize < 1 || gridSize > 100) {
     alert("O valor da grid deve estar entre 1 e 100");
   } else {
-    container.innerHTML = gridSize;
+    container.innerHTML = "";
+    for (let i = 0; i < gridSize; i++) {
+      const grid = document.createElement("div");
+      grid.classList.add("square");
+      container.appendChild(grid);
+    }
+    let squareList = document.querySelectorAll(".square");
+    squareList.forEach((square) => {
+      square.style.width = `calc(100vw/${gridSize})`;
+      square.style.height = square.style.width;
+    });
   }
 });
