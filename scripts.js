edbbd28339bc226bb64d9ce4e893698e9379a026
gpmacedo <gpmmacedo@hotmail.com>
2026-06-20 10:59:04 -0400

    Create a way to clear the pad

--- scripts.js
+++ scripts.js
@@ -1,5 +1,17 @@
+const container = document.querySelector(".grid-container");
+const btn = document.querySelector(".create-new");
+
 for (let i = 0; i < 16 * 16; i++) {
   const grid = document.createElement("div");
   grid.classList.add("square");
-  document.querySelector(".grid-container").appendChild(grid);
+  container.appendChild(grid);
 }
+
+btn.addEventListener("click", () => {
+  const gridSize = prompt("Escolha o tamanho da grid", "1-100");
+  if (gridSize < 1 || gridSize > 100) {
+    alert("O valor da grid deve estar entre 1 e 100");
+  } else {
+    container.innerHTML = gridSize;
+  }
+});
